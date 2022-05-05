25b9ae82aa7bcceb90111285a4f0beabb9c3f9a1
Lundqvist <theodor.lundqvist@gmail.com>
2022-05-05 22:08:36 +0200

    basic loader layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,37 @@ import 'dart:io';
 import 'dart:ui';
 
 //import 'package:desktop_window/desktop_window.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bridgestars_launcher/launcher.dart';
 import 'package:dart_vlc/dart_vlc.dart';
+import 'package:flutter/widgets.dart';
 import 'package:window_manager/window_manager.dart';
-import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
+
+//import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
+//import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
-  if (!kIsWeb && (Platform.isMacOS || Platform.isLinux || Platform.isWindows)) {
-
-  }
+  if (!kIsWeb &&
+      (Platform.isMacOS || Platform.isLinux || Platform.isWindows)) {}
   await windowManager.ensureInitialized();
   DartVLC.initialize();
 
   WindowOptions windowOptions = WindowOptions(
-    size: Size(1620/2, 1080/2),
+    size: Size(1620 / 2, 1080 / 2),
     center: true,
     backgroundColor: Colors.transparent,
     skipTaskbar: false,
-    minimumSize: Size(1620/4, 1080/4),
-    maximumSize: Size(1620/1.5, 1080/1.5),
+    minimumSize: Size(1620 / 4, 1080 / 4),
+    maximumSize: Size(1620 / 1.5, 1080 / 1.5),
     titleBarStyle: TitleBarStyle.hidden,
   );
-  await windowManager.setAspectRatio(16.0/9.0);
+  await windowManager.setAspectRatio(16.0 / 9.0);
   windowManager.waitUntilReadyToShow(windowOptions, () async {
     await windowManager.show();
     await windowManager.focus();
@@ -37,13 +41,13 @@ void main() async {
   runApp(LauncherApp());
 }
 
-class LauncherApp extends StatelessWidget{
+class LauncherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Bridgestars LauncherAAA',
-      theme:defaultTheme,
+      theme: defaultTheme,
       // theme: ThemeData(
       //   primarySwatch: Colors.blue,
       //   visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -54,7 +58,7 @@ class LauncherApp extends StatelessWidget{
 }
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key, this.title="asd"}) : super(key: key);
+  HomePage({Key? key, this.title = "asd"}) : super(key: key);
 
   final String title;
 
@@ -62,9 +66,7 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-
 class _HomePageState extends State<HomePage> with WindowListener {
-
   //#region events
   @override
   void onWindowFocus() {
@@ -72,12 +74,14 @@ class _HomePageState extends State<HomePage> with WindowListener {
     setState(() {});
     // do something
   }
+
   @override
   void initState() {
     windowManager.addListener(this);
     _init();
     super.initState();
   }
+
   @override
   void dispose() {
     windowManager.removeListener(this);
@@ -89,20 +93,20 @@ class _HomePageState extends State<HomePage> with WindowListener {
     print('[WindowManager] onWindowEvent: $eventName');
   }
 
-
   @override
   void onWindowClose() async {
     bool _isPreventClose = await windowManager.isPreventClose();
     if (_isPreventClose) {
       print("STOPPING");
-      if(showUI){
+      if (showUI) {
         player.stop();
         await playOutroBeforeHiding();
-      }
-      else player.stop();
+      } else
+        player.stop();
       await windowManager.destroy();
     }
   }
+
   //#endregion
 
   void _init() async {
@@ -113,17 +117,14 @@ class _HomePageState extends State<HomePage> with WindowListener {
   }
 
   void setMessage(String s) => setState(() {
-    message = s;
-  });
+        message = s;
+      });
+
   void setLauncherState(LauncherState s, DownloadInfo? p) => setState(() {
-    launcherState = s;
-    launcherStateString = getLauncherStateString(s);
-    progress = p;
-  });
-
-
-
-
+        launcherState = s;
+        launcherStateString = getLauncherStateString(s);
+        progress = p;
+      });
 
   String message = 'message';
   DownloadInfo? progress;
@@ -131,11 +132,11 @@ class _HomePageState extends State<HomePage> with WindowListener {
   LauncherState launcherState = LauncherState.waiting;
   String launcherStateString = getLauncherStateString(LauncherState.waiting);
   bool showUI = false;
-  var player = Player(id:124135);
+  bool showLoader = true;
+  var player = Player(id: 124135);
 
-
-  void showUIWhenVideoDone() async  {
-    await Future.delayed(const Duration(milliseconds: 2500), (){});
+  void showUIWhenVideoDone() async {
+    await Future.delayed(const Duration(milliseconds: 2500), () {});
     setState(() {
       showUI = true;
     });
@@ -148,20 +149,19 @@ class _HomePageState extends State<HomePage> with WindowListener {
     player.open(
       Media.asset('assets/shortOutro.mov'),
     );
-    await Future.delayed(const Duration(milliseconds: 850), (){});
+    await Future.delayed(const Duration(milliseconds: 850), () {});
     player.stop();
   }
 
-
   // downloading logic is handled by this method
-
 
   Future setup() async {
     print("setup");
     player.open(
       Media.asset('assets/shortIntro.mov'),
     );
-    Future.delayed(const Duration(milliseconds: 2500), (){}).then((value) => player.stop());
+    Future.delayed(const Duration(milliseconds: 2500), () {})
+        .then((value) => player.stop());
     showUIWhenVideoDone();
     //stopVideoWhenDone();
     //launcher = await Launcher.create(setLauncherState);
@@ -172,15 +172,13 @@ class _HomePageState extends State<HomePage> with WindowListener {
     //});
     //TODO Check version
   }
-  
-
 
   Future handleBtnPress() async {
-    if(launcher == null) return;
+    if (launcher == null) return;
     await launcher!.handleBtnPress();
   }
 
-  _HomePageState(){
+  _HomePageState() {
     setup();
   }
 
@@ -188,11 +186,16 @@ class _HomePageState extends State<HomePage> with WindowListener {
 
   // which will be used to save the file to that path in the downloadFile method
 
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context)
+        .size
+        .width; // * MediaQuery.of(context).devicePixelRatio;
+    var height = MediaQuery.of(context)
+        .size
+        .height; // * MediaQuery.of(context).devicePixelRatio;
 
-    @override
-    Widget build(BuildContext context) {
-      print('build running');
-      return Scaffold(
+    return Scaffold(
         // floatingActionButton: FloatingActionButton(
         //   onPressed: () {  },
         //   backgroundColor: defaultTheme.scaffoldBackgroundColor,
@@ -200,130 +203,44 @@ class _HomePageState extends State<HomePage> with WindowListener {
         //
         // ),
         body: Stack(
-          children: [
-             Video(
-               player: player,
-               //height: 360,
-               //width: 640,
-               scale: 1.0, // default
-               showControls: false, // default
-               playlistLength: 1,
-             ),
-            if (showUI) Container(
-             child: Padding(
-               padding: const EdgeInsets.only(top: 0),
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                 children: <Widget>[
-                   Padding(
-                     padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                     child: LinearProgressIndicator(
-                       minHeight: 5,
-                       value: 0,//progress != null ? progress!.percentDone/100 : 0.0,
-                       color: defaultTheme.accentColor,
-                     ),
-                   ),
-                   SizedBox(
-                     width: 400,
-                     height: 200,
-                     child: Column(
-                       mainAxisAlignment: MainAxisAlignment.center,
-                       children: <Widget>[
-                         Text(progress.toString()),
-                         Text(message, style: TextStyle(color: defaultTheme.accentColor))
-                       ],
+      children: [
+        Video(
+          player: player,
+          //height: 360,
+          //width: 640,
+          scale: 1.0, // default
+          showControls: false, // default
+          playlistLength: 1,
+        ),
+        if (showUI)
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(height: height*0.5),
+              if (showLoader) Container(
+                width: width/20,
+                height: width/20,
+                alignment: Alignment.center,
+                child: LoadingIndicator(
+                  indicatorType: Indicator.ballRotateChase,
 
-                     ),
-                   ),
-                   Stack(
-                     children: [
-                       Positioned(
-                           left: 10,//-window.physicalSize.width/4,
-                           bottom: 10,
-                           child: Text("asd"),//launcher!.localAppVersion!.getNbr().toString(), style: TextStyle(fontSize: 14),)
-                         // child: new Container(
-                         //   width: 100.0,
-                         //   height: 80.0,
-                         //   decoration: new BoxDecoration(color: Colors.white),
-                         //   child: new Text('hello'),
-                         // )
-                       ),
+                  /// Required, The loading type of the widget
+                  colors: const [Colors.white],
 
-                       Padding(
+                  /// Optional, The color collections
 
-                         padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                         child: Row(
-                           mainAxisAlignment: MainAxisAlignment.center,
-                           children: <Widget>[
-                             Opacity(
-                               opacity: 0,
-                               child: TextButton(
-                                   child: Text("Uninstall"),
-                                   onPressed: () {}//launcher.handleBtnPress(),
-                               ),
-                             ),
-
-                             // if(launcher != null) Positioned(
-                             //   child: Text(launcher!.localAppVersion.toString(), style: TextStyle(color: Colors.white),),
-                             //   left: 30,
-                             //   top: 30
-                             // ),
-
-                             ElevatedButton(
-                               style: ElevatedButton.styleFrom(
-                                 //elevation: 100
-                                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                                   primary: defaultTheme.accentColor,
-                                   minimumSize: Size(250, 75)
-                               ),
-
-                               child: Text(launcherStateString, style: TextStyle(fontSize: 24),),
-                               onPressed: () async {
-                                 try{
-                                   await handleBtnPress();
-                                 }
-                                 catch (e) {
-                                   setMessage(e.toString());
-                                   launcher?.updateState();
-                                   throw Exception(e);
-                                 }
-                               },//launcher.handleBtnPress(),
-                             ),
-                             TextButton(
-
-                               child: Text("Uninstall", style: TextStyle(color: Colors.white),),
-                               onPressed: () async {
-                                 try{
-                                   showAlertDialog(context, "Alert", "Are you sure you want to uninstall?",[Text("Cancel"), Text("Uninstall", style: TextStyle(color: Colors.red),)], [() => {}, () async {
-                                     await launcher?.uninstall();
-                                     launcher?.updateState();
-                                   }]);
-                                 }
-                                 catch (e) {
-                                   setMessage(e.toString());
-                                   throw Exception(e);
-                                 }
-                               },//launcher.handleBtnPress(),
-                             ),
-
-                           ],
-                         ),
-                       )
-                     ],
-                   )
-
-                 ],
-               ),
-             ),
-            ),
-          ],
-        )
-
-
-        );
-
-    }
+                  //strokeWidth: 2, /// Optional, The stroke of the line, only applicable to widget which contains line
+                  //backgroundColor: Colors.black,      /// Optional, Background of the widget
+                  //pathBackgroundColor: Colors.black
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: height/50)
+                  ,child: Text(launcherStateString, style: TextStyle(fontSize: width/60)))
+            ])
+          ])
+      ],
+    ));
   }
+}
 
 String getLauncherStateString(LauncherState s) {
   switch (s) {
@@ -350,24 +267,26 @@ String getLauncherStateString(LauncherState s) {
   }
 }
 
-showAlertDialog(BuildContext context, String title, String message, List<Text> btnTexts, List<Function()> btnActions) {
+showAlertDialog(BuildContext context, String title, String message,
+    List<Text> btnTexts, List<Function()> btnActions) {
+  if (btnTexts.length != btnActions.length)
+    throw new ArgumentError("nbr of actions must be equal to nbr of btn texts");
 
-  if(btnTexts.length != btnActions.length) throw new ArgumentError("nbr of actions must be equal to nbr of btn texts");
+  var btns = btnActions.asMap().entries.map((e) => TextButton(
+      onPressed: () {
+        Navigator.of(context).pop(); //pop dialog
+        e.value(); //run btnAction
+      },
+      child: btnTexts.elementAt(e.key)));
 
-  var btns = btnActions.asMap().entries.map((e) => TextButton(onPressed: () {
-      Navigator.of(context).pop(); //pop dialog
-      e.value(); //run btnAction
-    }, child: btnTexts.elementAt(e.key)));
-
-
-  // set up the AlertDialog
+// set up the AlertDialog
   AlertDialog alert = AlertDialog(
     title: Text(title),
     content: Text(message),
     actions: btns.toList(),
   );
 
-  // show the dialog
+// show the dialog
   showDialog(
     context: context,
     builder: (BuildContext context) {
@@ -376,19 +295,20 @@ showAlertDialog(BuildContext context, String title, String message, List<Text> b
   );
 }
 
-
-PopupMenuItem _buildPopupMenuItem(
-      String title, {IconData iconData = Icons.print}) {
-    return PopupMenuItem(
-     child: Row(
-        children: [
-          Icon(iconData, color: Colors.black,),
-          Text(title),
-        ],
-      ),
-    );
-  }
-
+PopupMenuItem _buildPopupMenuItem(String title,
+    {IconData iconData = Icons.print}) {
+  return PopupMenuItem(
+    child: Row(
+      children: [
+        Icon(
+          iconData,
+          color: Colors.black,
+        ),
+        Text(title),
+      ],
+    ),
+  );
+}
 
 ThemeData defaultTheme = ThemeData(
   brightness: Brightness.light,
@@ -434,3 +354,22 @@ ThemeData defaultTheme = ThemeData(
     ),
   ),
 );
+
+// UNINSTALL DIALOG
+//
+// TextButton(
+//
+// child: Text("Uninstall", style: TextStyle(color: Colors.white),),
+//   onPressed: () async {
+//   try{
+//     showAlertDialog(context, "Alert", "Are you sure you want to uninstall?",[Text("Cancel"), Text("Uninstall", style: TextStyle(color: Colors.red),)], [() => {}, () async {
+//       await launcher?.uninstall();
+//       launcher?.updateState();
+//     }]);
+//   }
+//   catch (e) {
+//     setMessage(e.toString());
+//     throw Exception(e);
+//     }
+//   },//launcher.handleBtnPress(),
+// ),
