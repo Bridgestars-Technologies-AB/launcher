a93e34f822840858538452fb6cd846bdec2552fe
Lundqvist <theodor.lundqvist@gmail.com>
2022-06-03 09:49:04 +0200

    add mac build scripts and change windows com.example to net.bridegstars

--- lib/launcher.dart
+++ lib/launcher.dart
@@ -1,10 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:bridgestars/dialogs.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
 import 'package:dio/dio.dart';
 import 'package:path_provider/path_provider.dart';
