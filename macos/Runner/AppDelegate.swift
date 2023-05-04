7168f8f76bb0f649425a4b4e4363724f6e87f467
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2023-05-04 22:14:54 +0200

    ask to move

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -6,4 +6,10 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+    override func applicationWillFinishLaunching(_ aNotification: Notification) {
+
+    }
+    override func applicationDidFinishLaunching(_ aNotification: Notification) {
+        LetsMove.shared.moveToApplicationsFolderIfNecessary()
+    }
 }
